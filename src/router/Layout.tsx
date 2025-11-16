48840e63e8ff72550327409007b8f109ba900ecf
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-16 11:34:59 +0900

    styles: main tag 스타일 수정

--- src/router/Layout.tsx
+++ src/router/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
-    <main className='mx-auto min-h-dvh max-w-[60rem]'>
+    <main>
       <Outlet />
     </main>
   );
