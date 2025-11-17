22f3e12d38850d23a988fed1314c02a266ec7e02
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-17 23:33:10 +0900

    feat: svg 추가, 폰트 추가, main bg 변경

---
+++ src/shared/assets/components/IconKebab.tsx
@@ -0,0 +1,36 @@
+/**
+ * ⚠️ 자동 생성된 파일입니다. 직접 수정하지 마세요.
+ */
+
+import type { SVGProps } from 'react';
+
+const IconKebab = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <mask
+      id="prefix__a"
+      width={24}
+      height={24}
+      x={0}
+      y={0}
+      maskUnits="userSpaceOnUse"
+      style={{
+        maskType: 'alpha',
+      }}
+    >
+      <path fill="currentColor" d="M0 0h24v24H0z" />
+    </mask>
+    <g mask="url(#prefix__a)">
+      <path
+        fill="currentColor"
+        d="M12 18.538a.96.96 0 0 1-.706-.293.96.96 0 0 1-.294-.707q0-.412.294-.706a.96.96 0 0 1 .706-.294q.412 0 .706.294a.96.96 0 0 1 .294.706q0 .412-.294.707a.96.96 0 0 1-.706.293M12 13a.96.96 0 0 1-.706-.294A.96.96 0 0 1 11 12q0-.412.294-.706A.96.96 0 0 1 12 11q.412 0 .706.294A.96.96 0 0 1 13 12q0 .412-.294.706A.96.96 0 0 1 12 13m0-5.539a.96.96 0 0 1-.706-.293A.96.96 0 0 1 11 6.46q0-.412.294-.706A.96.96 0 0 1 12 5.461q.412 0 .706.294a.96.96 0 0 1 .294.706q0 .412-.294.707A.96.96 0 0 1 12 7.46"
+      />
+    </g>
+  </svg>
+);
+export default IconKebab;
