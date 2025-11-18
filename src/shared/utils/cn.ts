198c1a2c5731c2155e2b2c6803e98a71442183e6
sminha <sminha8590@naver.com>
2025-11-18 21:38:45 +0900

    feat: cn 함수에 text-button 관련 설정 추가

--- src/shared/utils/cn.ts
+++ src/shared/utils/cn.ts
@@ -1,6 +1,16 @@
 import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
+
+const twMergeCustom = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': [{ text: ['button1', 'button2', 'button3', 'button4'] }],
+      'font-weight': [{ text: ['button1', 'button2', 'button3', 'button4'] }],
+      'leading': [{ text: ['button1', 'button2', 'button3', 'button4'] }],
+    }
+  }
+})
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMergeCustom(clsx(inputs));
 }
