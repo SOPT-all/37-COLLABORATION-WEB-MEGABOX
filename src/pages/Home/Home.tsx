60167a127d2d0774e33af8be0260ab2403fe743e
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-19 17:30:25 +0900

    feat: home 구현

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,22 @@
+import Divider from '@components/divider/Divider';
+import { MOVIES } from '@constants/movies';
+import Carousel from '@pages/Home/components/Carousel';
+
 export default function Home() {
-  return <div></div>;
+  const item = MOVIES.map(movie => ({ id: movie.id, image: movie.image }));
+  const handleClickItem = (id: number) => {
+    console.info(id);
+  };
+
+  return (
+    <div>
+      {/* header 영역 */}
+      {/* main 영역 */}
+      <div className='h-[50rem]' />
+      <div className='flex flex-col gap-[1rem] px-[1.7rem]'>
+        <Divider />
+        <Carousel items={item} handleClickItem={handleClickItem} />
+      </div>
+    </div>
+  );
 }
