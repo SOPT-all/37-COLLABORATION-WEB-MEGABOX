49b2cc7ac5dda98a84311f59cc8352a265ef7d1d
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-19 10:17:26 +0900

    feat: home 에 예시코드 작성

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,18 @@
-import { IconStarFill } from '@assets/index';
-import Review from '@components/review/Review';
+import Movie from '@components/movie/Movie';
 
 export default function Home() {
   return (
     <div>
-      <img src={'../../../public/assets/img/img-toss-logo.png'} />
+      <Movie
+        id={1}
+        title='나우 유 씨 미 3'
+        tag='#애니메이션 #액션'
+        ageRating={15}
+        releaseDate='2024-11-15'
+        runningTimeMinutes={100}
+        handleClickCard={() => {}}
+        className='cursor-pointer'
+      />
     </div>
   );
 }
