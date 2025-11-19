29a1c6e09d60025f9a0a05cca73944dea1977523
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-19 16:29:10 +0900

    code review: aria-hidden='true' 추가

--- src/shared/components/divider/Divider.tsx
+++ src/shared/components/divider/Divider.tsx
@@ -7,6 +7,7 @@ export default function Divider() {
         viewBox='0 0 344 12'
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
+        aria-hidden='true'
       >
         <ellipse
           cx='172.5'
