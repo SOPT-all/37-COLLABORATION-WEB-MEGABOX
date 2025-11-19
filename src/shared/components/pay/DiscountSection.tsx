3edd18d907f01efdb1088d90b2991b1825d8450d
huniversal <hunivers0523@naver.com>
2025-11-20 02:46:15 +0900

    refactor: 탭 버튼에 cn 함수 적용

--- src/shared/components/pay/DiscountSection.tsx
+++ src/shared/components/pay/DiscountSection.tsx
@@ -1,9 +1,19 @@
 import { useState } from 'react'
+import { cn } from "@utils/cn";
 
 type TabType = 'coupon' | 'point';
 
 export const DiscountSection = () => {
   const [activeTab, setActiveTab] = useState<TabType | null>(null);
+
+  const getTabClassName = (isActive: boolean) =>
+    cn(
+      'font-button2 relative flex-1 rounded-[0.4rem] border-[0.1rem] p-[1rem]',
+      isActive
+        ? 'border-violet-600 text-violet-600'
+        : 'border-gray-300 text-gray-900'
+    );
+
   return (
     <section className='bg-white p-[1.6rem]'>
       <div className='mb-[2.5rem] flex items-center justify-between'>
@@ -13,24 +23,16 @@ export const DiscountSection = () => {
         </span>
       </div>
       {/* 탭 버튼 */}
-      <div className='gap-[0.7rem] mb-[1.8rem] flex'>
+      <div className='mb-[1.8rem] flex gap-[0.7rem]'>
         <button
           onClick={() => setActiveTab('coupon')}
-          className={`font-button2 relative flex-1 rounded-[0.4rem] border-[0.1rem] p-[1rem] ${
-            activeTab === 'coupon'
-              ? 'border-violet-600 text-violet-600'
-              : 'border-gray-300 text-gray-900'
-          }`}
+          className={getTabClassName(activeTab === 'coupon')}
         >
           쿠폰/관람권/기타
         </button>
         <button
           onClick={() => setActiveTab('point')}
-          className={`font-button2 relative flex-1 rounded-[0.4rem] border-[0.1rem] p-[1rem] ${
-            activeTab === 'point'
-              ? 'border-violet-600 text-violet-600'
-              : 'border-gray-300 text-gray-900'
-          }`}
+          className={getTabClassName(activeTab === 'point')}
         >
           포인트
         </button>
