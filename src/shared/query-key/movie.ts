8ab0e27f8e22ec200afc34940d36a4232164074b
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-19 10:57:47 +0900

    refactor: 영화관 쿼리키에 추가

--- src/shared/query-key/movie.ts
+++ src/shared/query-key/movie.ts
@@ -2,5 +2,10 @@ export const MOVIE_KEY = {
   ALL: ['movies'],
   LIST: () => [...MOVIE_KEY.ALL, 'list'],
   DETAILS: () => [...MOVIE_KEY.ALL, 'detail'],
-  DETAIL: (id: number) => [...MOVIE_KEY.DETAILS(), id],
+  DETAIL: (movieId: number) => [...MOVIE_KEY.DETAILS(), movieId],
+  THEATERS: (movieId: number) => [...MOVIE_KEY.DETAIL(movieId), 'theaters'],
+  THEATER: (movieId: number, theaterId: number) => [
+    ...MOVIE_KEY.THEATERS(movieId),
+    theaterId,
+  ],
 } as const;
