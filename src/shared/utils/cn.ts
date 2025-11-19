7d439318542c7c8b4f5d6570d2c9719c645f141f
sminha <sminha8590@naver.com>
2025-11-19 15:50:14 +0900

    refactor: cn 함수 복구

--- src/shared/utils/cn.ts
+++ src/shared/utils/cn.ts
@@ -1,16 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
-import { extendTailwindMerge } from "tailwind-merge";
-
-const twMergeCustom = extendTailwindMerge({
-  extend: {
-    classGroups: {
-      'font-size': [{ text: ['button1', 'button2', 'button3', 'button4'] }],
-      'font-weight': [{ text: ['button1', 'button2', 'button3', 'button4'] }],
-      'leading': [{ text: ['button1', 'button2', 'button3', 'button4'] }],
-    }
-  }
-})
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMergeCustom(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
