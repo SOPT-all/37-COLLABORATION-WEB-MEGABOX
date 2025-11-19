c0337a7a7700bd15b98d3a0e6726ffc01b1e9b6f
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-19 10:17:13 +0900

    feat: movie 컴포넌트 구현

---
+++ src/shared/components/movie/Movie.tsx
@@ -0,0 +1,84 @@
+import { MOVIES } from '@constants/movies';
+import ImgBtBolby from '@/../public/assets/img-bt-bolby.png';
+import ImgBtPlay from '@/../public/assets/img-bt-4d.png';
+import { cn } from '@utils/cn';
+
+interface MovieProps {
+  id: keyof typeof MOVIES;
+  title: string;
+  tag: string;
+  ageRating: number;
+  releaseDate: string;
+  runningTimeMinutes: number;
+  className?: string;
+  handleClickCard?: () => void;
+}
+export default function Movie({
+  id,
+  title,
+  tag,
+  ageRating,
+  releaseDate,
+  runningTimeMinutes,
+  className,
+  handleClickCard,
+}: MovieProps) {
+  const movie = MOVIES[id];
+
+  return (
+    <div
+      className={cn(
+        'relative h-[26.4rem] w-full bg-cover bg-center bg-no-repeat',
+        className
+      )}
+      style={{
+        backgroundImage: `url(${movie.backgroundImage})`,
+      }}
+      aria-label={`${title} 영화 정보`}
+      onClick={handleClickCard ?? undefined}
+    >
+      <div className='gradient-movie-overlay absolute inset-0' />
+      <div className='relative z-10 flex shrink-0 flex-col gap-[0.8rem] px-[3.1rem] py-[2rem]'>
+        <div className='flex h-full items-end gap-[2rem]'>
+          <img
+            src={movie.image}
+            alt={`${title} 포스터 이미지`}
+            className='h-[17.4rem] w-[12.4rem]'
+          />
+          <div className='flex flex-col gap-[1.9rem]'>
+            <div className='flex flex-col gap-[0.5rem]'>
+              <h1 className='text-headline1 text-gray-0'>{title}</h1>
+              <p className='text-label1 text-violet-200'>{tag}</p>
+            </div>
+            <div className='flex flex-col gap-[0.3rem]'>
+              <span className='text-caption2 text-gray-0'>
+                {ageRating}세이상관람가
+              </span>
+              <div className='flex gap-[0.3rem]'>
+                <span className='text-label1 text-gray-0'>
+                  {releaseDate} 개봉
+                </span>
+                <span className='text-label1 text-gray-0'>·</span>
+                <span className='text-label1 text-gray-0'>
+                  {runningTimeMinutes}분
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='flex gap-[0.5rem]'>
+          <img
+            src={ImgBtBolby}
+            alt={`${title} 볼비 버튼`}
+            className='w-[6.9rem]'
+          />
+          <img
+            src={ImgBtPlay}
+            alt={`${title} 재생 버튼`}
+            className='w-[5rem]'
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
