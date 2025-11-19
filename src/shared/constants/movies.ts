15feee3abdfbb446e8c46058ca384412086a835b
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-19 10:16:28 +0900

    refactor: moives 상수 객체로 변환 및 key type 정의

--- src/shared/constants/movies.ts
+++ src/shared/constants/movies.ts
@@ -1,51 +1,46 @@
-export const MOVIES = [
-  {
-    id: 1,
+export const MOVIES = Object.freeze({
+  1: {
     title: '나우 유 씨 미 3',
     categories: ['나우 유 씨 미 3'],
     image: '/assets/@movies/img-moviePoster.png',
-    backgroundImage: '/assets/@movies/img-moviePoster.png',
+    backgroundImage: '/assets/@movies/img-stillcut1.png',
   },
-  {
-    id: 2,
+  2: {
     title: '극장판 체인소 맨: 레제편',
     categories: ['극장판 체인소 맨: 레제편'],
     image: '/assets/@movies/img-moviePoster.png',
     backgroundImage: '/assets/@movies/img-moviePoster-chainman.png',
   },
-  {
-    id: 3,
+  3: {
     title: '극장판 주술회전: 회옥·옥절',
     categories: ['극장판 주술회전: 회옥·옥절'],
     image: '/assets/@movies/img-moviePoster.png',
     backgroundImage: '/assets/@movies/img-moviePoster-jujutsuKaisen.png',
   },
-  {
-    id: 4,
+  4: {
     title: '퍼스트 라이드',
     categories: ['퍼스트 라이드'],
     image: '/assets/@movies/img-moviePoster.png',
     backgroundImage: '/assets/@movies/img-moviePoster-firstRide.png',
   },
-  {
-    id: 5,
+  5: {
     title: '극장판 귀멸의 칼날: 무한성편',
     categories: ['극장판 귀멸의 칼날: 무한성편'],
     image: '/assets/@movies/img-moviePoster.png',
     backgroundImage: 'https://via.placeholder.com/150',
   },
-  {
-    id: 6,
+  6: {
     title: '위키드',
     categories: ['위키드'],
     image: '/assets/@movies/img-moviePoster.png',
     backgroundImage: 'https://via.placeholder.com/150',
   },
-  {
-    id: 7,
+  7: {
     title: '프레데터: 죽음의 땅',
     categories: ['프레데터: 죽음의 땅'],
     image: '/assets/@movies/img-moviePoster.png',
     backgroundImage: 'https://via.placeholder.com/150',
   },
-] as const;
+});
+
+export type Movie = (typeof MOVIES)[keyof typeof MOVIES];
