7dd03abb35a6cc311d813bc47e09d98e31c0c091
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-20 12:28:05 +0900

    feat: header, footer 구현

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,40 @@
+import { Divider, Movie } from '@components/index';
+import Carousel from '@/pages/home/components/Carousel';
+import { useMovie } from '@/pages/home/hooks';
+import Header from '@/shared/components/header/Header';
+import Button from '@/shared/components/button/Button';
+
+export default function Home() {
+  const { selectedMovie, item, handleClickItem, handleClickCard } = useMovie();
+
+  return (
+    <div>
+      {/* header 영역 */}
+      <Header variant='main' />
+
+      {selectedMovie && (
+        <Movie
+          key={selectedMovie.id}
+          id={selectedMovie.id}
+          title={selectedMovie.title}
+          tag={selectedMovie.tag}
+          ageRating={selectedMovie.ageRating}
+          releaseDate={selectedMovie.releaseDate}
+          runningTimeMinutes={selectedMovie.runningTimeMinutes}
+          className='mb-[0.9rem] cursor-pointer'
+          handleClickCard={handleClickCard}
+        />
+      )}
+
+      <div className='flex flex-col gap-[1rem] px-[1.7rem]'>
+        <Divider />
+        <Carousel items={item} handleClickItem={handleClickItem} />
+      </div>
+      <footer className='fixed-center right-0 bottom-0 left-0 px-[1.7rem] pb-[4.9rem]'>
+        <Button variant='primary' onClick={handleClickCard}>
+          바로 예매 하기
+        </Button>
+      </footer>
+    </div>
+  );
+}
