dd162c0a5117f6efb6c456370723b8b2249215ad
huniversal <hunivers0523@naver.com>
2025-11-21 03:59:02 +0900

    style: 결제 수단 그리드 스타일링

--- src/shared/components/payment/Payment.tsx
+++ src/shared/components/payment/Payment.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import { cn } from "@utils/cn";
 import UppArrow from '@assets/components/IconSystemUparrow';
 import IconItemPlus from '@assets/components/IconItemPlus';
+import IconCard from '@assets/components/IconCard';
+import IconSimpleCard from '@assets/components/IconSimpleCard';
+import Phone from '@assets/components/IconPhone';
+import MyCard from '@assets/components/IconMyCard';
 
 type PaymentMethodType =
   | 'credit-card'
@@ -10,10 +14,10 @@ type PaymentMethodType =
   | 'account-pay';
 
 const PAYMENT_METHODS = [
-  { id: 'credit-card' as const, label: '신용카드' },
-  { id: 'simple-pay' as const, label: '간편결제' },
-  { id: 'phone-pay' as const, label: '휴대폰결제' },
-  { id: 'account-pay' as const, label: '내통장결제' },
+  { id: 'credit-card' as const, label: '신용카드', icon: IconCard },
+  { id: 'simple-pay' as const, label: '간편결제', icon: IconSimpleCard },
+  { id: 'phone-pay' as const, label: '휴대폰결제', icon: Phone },
+  { id: 'account-pay' as const, label: '내통장결제', icon: MyCard },
 ];
 
 const Payment = () => {
@@ -59,29 +63,39 @@ const Payment = () => {
           </div>
           {/* 결제 방법 그리드 */}
           <div className='mb-[1.6rem] grid grid-cols-2 gap-[1.2rem]'>
-            {PAYMENT_METHODS.map(method => (
-              <button
-                key={method.id}
-                onClick={() => handlePaymentMethodClick(method.id)}
-                className={cn(
-                  'flex flex-col items-center gap-[1.2rem] rounded-[0.4rem] border p-[2.4rem]',
-                  selectedMethod === method.id
-                    ? 'border-violet-600'
-                    : 'border-gray-300'
-                )}
-              >
-                <span
+            {PAYMENT_METHODS.map((method) => {
+              const Icon = method.icon;
+              return (
+                <button
+                  key={method.id}
+                  onClick={() => handlePaymentMethodClick(method.id)}
                   className={cn(
-                    'font-body2',
+                    'flex flex-col items-center gap-[1.2rem] rounded-[0.4rem] border px-[1rem] py-[1.2rem]',
                     selectedMethod === method.id
-                      ? 'text-violet-600'
-                      : 'text-gray-700'
+                      ? 'border-violet-600'
+                      : 'border-gray-300'
                   )}
                 >
-                  {method.label}
-                </span>
-              </button>
-            ))}
+                  <Icon width={30} height={30}
+                    className={cn(
+                      selectedMethod === method.id
+                        ? 'text-violet-600'
+                        : 'text-gray-700'
+                    )}
+                  />
+                  <span
+                    className={cn(
+                      'font-button2',
+                      selectedMethod === method.id
+                        ? 'text-violet-600'
+                        : 'text-gray-700'
+                    )}
+                  >
+                    {method.label}
+                  </span>
+                </button>
+              )
+            })}
           </div>
 
           {/* toss 카드 */}
