5cdcf9570af43429e576f14aae28e67a5170b552
sminha <sminha8590@naver.com>
2025-11-21 21:44:34 +0900

    feat: 필요 상수 정의

---
+++ src/pages/booking/constants/index.ts
@@ -0,0 +1,3 @@
+export { TIMES } from '@pages/booking/constants/times';
+export { CINEMAS } from '@pages/booking/constants/cinemas';
+export { TIME_RANGES } from '@pages/booking/constants/time-ranges';
