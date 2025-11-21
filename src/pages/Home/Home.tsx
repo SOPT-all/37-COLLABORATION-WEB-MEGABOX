ece15011d43541373c31ab678c3685c8d7ec1371
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-21 10:41:32 +0900

    feat: home 에 적용

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,6 +5,7 @@ import { useModal } from '@components/@modal/hooks/use-modal';
 import Divider from '@components/divider/Divider';
 import Header from '@components/header/Header';
 import Movie from '@components/movie/Movie';
+import Spinner from '@components/spinner/Spinner';
 
 export default function Home() {
   const maxQuantity = 10;
@@ -18,38 +19,38 @@ export default function Home() {
   return (
     <>
       {/* 영화 헤더 테스트: 테스트 후 삭제 예정 */}
-      <Header variant="main" />
-      <Header variant="movie" title="무비 제목" handleClickBack={handleBack} />
-
+      <Header variant='main' />
+      <Header variant='movie' title='무비 제목' handleClickBack={handleBack} />
+      <Spinner />
       <div>
-        <div className="p-8">
+        <div className='p-8'>
           <Divider />
           {/* TODO : 테스트 후 삭제 예정 */}
           {/* 텍스트 색상 테스트 */}
-          <h1 className="mb-4 font-bold text-violet-700">Violet 700</h1>
-          <h1 className="mb-4 font-bold text-violet-600">Violet 600</h1>
-          <h1 className="mb-4 font-bold text-violet-500">Violet 500</h1>
-          <h1 className="mb-4 font-bold text-violet-400">Violet 400</h1>
-          <h1 className="mb-4 font-bold text-violet-300">Violet 300</h1>
-          <h1 className="mb-8 font-bold text-violet-200">Violet 200</h1>
-          <h1 className="mb-8 font-bold text-red-500">Sub-Red-200</h1>
-          <h1 className="mb-8 font-bold text-blue-500">Sub-Blue-200</h1>
-          <h1 className="mb-8 font-bold text-blueGreen-500">
+          <h1 className='mb-4 font-bold text-violet-700'>Violet 700</h1>
+          <h1 className='mb-4 font-bold text-violet-600'>Violet 600</h1>
+          <h1 className='mb-4 font-bold text-violet-500'>Violet 500</h1>
+          <h1 className='mb-4 font-bold text-violet-400'>Violet 400</h1>
+          <h1 className='mb-4 font-bold text-violet-300'>Violet 300</h1>
+          <h1 className='mb-8 font-bold text-violet-200'>Violet 200</h1>
+          <h1 className='mb-8 font-bold text-red-500'>Sub-Red-200</h1>
+          <h1 className='mb-8 font-bold text-blue-500'>Sub-Blue-200</h1>
+          <h1 className='text-blueGreen-500 mb-8 font-bold'>
             Sub-blueGreen-200
           </h1>
 
           {/* 그라디언트 테스트 */}
-          <div className="gradient-1 mb-4 h-64 w-64 rounded-3xl" />
-          <div className="gradient-2 mb-4 h-64 w-64 rounded-3xl" />
-          <div className="gradient-3 h-64 w-64 rounded-3xl" />
+          <div className='gradient-1 mb-4 h-64 w-64 rounded-3xl' />
+          <div className='gradient-2 mb-4 h-64 w-64 rounded-3xl' />
+          <div className='gradient-3 h-64 w-64 rounded-3xl' />
         </div>
-        <IconStarFill className="text-red-500" />
-        <div className="p-[2rem]">
+        <IconStarFill className='text-red-500' />
+        <div className='p-[2rem]'>
           <Review
-            content="영화를 2회차 시청하는 건 이 작품이 첨입니다. 너무 재밌어요. 초반엔 달달한 로맨스를 풍기다 전투씬부터는 화려하고 역동작인 액션씬이라 보는 재미가 있었습니다. 만족 100입니다"
+            content='영화를 2회차 시청하는 건 이 작품이 첨입니다. 너무 재밌어요. 초반엔 달달한 로맨스를 풍기다 전투씬부터는 화려하고 역동작인 액션씬이라 보는 재미가 있었습니다. 만족 100입니다'
             rating={4.3}
             createdAt={new Date()}
-            nickname="test"
+            nickname='test'
           />
         </div>
       </div>
