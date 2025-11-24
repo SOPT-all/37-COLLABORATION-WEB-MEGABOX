e67b55e573f4415191edaf82288cbe1c04d5258b
woodzverse <woodzverse@naver.com>
2025-11-24 21:57:16 +0900

    fix: build 오류 해결

--- src/pages/movie-detail/constants/info-mock.ts
+++ src/pages/movie-detail/constants/info-mock.ts
@@ -1,8 +1,8 @@
-import ImgTrailer1 from '@/../public/assets/@movies/img-trailer.png';
-import ImgTrailer2 from '@/../public/assets/@movies/img-trailer2.png';
-import ImgTrailer3 from '@/../public/assets/@movies/img-trailer3.png';
-import ImgStillCut from '@/../public/assets/@movies/img-stillcut1.png';
-import ImgGoods from '@/../public/assets/@movies/img-goods.png';
+const ImgTrailer1 = '/assets/@movies/img-trailer.png';
+const ImgTrailer2 = '/assets/@movies/img-trailer2.png';
+const ImgTrailer3 = '/assets/@movies/img-trailer3.png';
+const ImgStillCut = '/assets/@movies/img-stillcut1.png';
+const ImgGoods = '/assets/@movies/img-goods.png';
 
 const TRAILERS = [
   ImgTrailer1,
@@ -11,7 +11,9 @@ const TRAILERS = [
   ImgTrailer1,
   ImgTrailer2,
 ];
+
 const POSTERS = [ImgStillCut, ImgStillCut, ImgStillCut];
+
 const GOODS = { title: '<체인소 맨> 특별 포스터', image: ImgGoods };
 
 export { TRAILERS, POSTERS, GOODS };
