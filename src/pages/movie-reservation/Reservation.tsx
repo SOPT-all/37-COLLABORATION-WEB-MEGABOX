bea07ef8d9c536d90463f717905561e29fefd7e5
민하 <minha@minhaui-noteubug.local>
2025-11-25 14:14:23 +0900

    feat: Reservation 컴포넌트 section으로 분리

--- src/pages/movie-reservation/Reservation.tsx
+++ src/pages/movie-reservation/Reservation.tsx
@@ -79,116 +79,51 @@ export default function Reservation() {
         title='영화 예매하기'
         handleClickBack={() => navigate(-1)}
       />
-
       <div className='p-[2rem]'>
         <div className='scroll-fade flex w-full flex-col items-start gap-[1.2rem]'>
           <Carousel
             selectedMovieIds={selectedMovieIds}
             handleClick={id => handleClickMovie(id)}
           />
-          <div className='scrollbar-hide flex w-full gap-[0.7rem] overflow-x-scroll px-[0.5rem]'>
-            {CINEMAS.map(cinema => (
-              <Chip
-                key={cinema}
-                variant='cinema'
-                isSelected={selectedCinemas.includes(cinema)}
-              >
-                {cinema}
-              </Chip>
-            ))}
-          </div>
-
-          <div className='scrollbar-hide flex w-full gap-[0.7rem] overflow-x-scroll px-[0.5rem]'>
-            {dates.map((dateInfo, idx) => {
-              const dayColorClass = cn(
-                dateInfo.day === '토' && 'text-blue-500',
-                dateInfo.day === '일' && 'text-red-500'
-              );
-              const dateString = dayjs(dateInfo.date).format('M/D');
-
-              return (
-                <Chip
-                  key={idx}
-                  variant='date'
-                  isSelected={selectedDate.date === dateInfo.date}
-                  onClick={() => handleClickDate(dateInfo)}
-                >
-                  <span className={cn('font-title3', dayColorClass)}>
-                    {dateInfo.day}
-                  </span>
-                  <span className={cn('font-label1', dayColorClass)}>
-                    {dateString}
-                  </span>
-                </Chip>
-              );
-            })}
-          </div>
+          <CinemaChips selectedCinemas={selectedCinemas} />
+          <DateChips
+            dates={dates}
+            selectedDate={selectedDate}
+            handleClickDate={handleClickDate}
+          />
         </div>
-
         <div className='mt-[1.8rem] mb-[1.6rem]'>
           <Divider />
         </div>
-
         <div className='flex w-full flex-col items-start gap-[1.1rem]'>
-          <div className='scrollbar-hide flex w-full items-start gap-[0.8rem] overflow-x-scroll py-[1rem] opacity-70'>
-            {TIMES.map((_, idx) => {
-              return (
-                <Chip
-                  key={idx}
-                  variant='time'
-                  isSelected={selectedTimeId === idx}
-                  onClick={() => handleClickTime(idx)}
-                >
-                  {TIMES[idx].label}
-                </Chip>
-              );
-            })}
-          </div>
-
-          {isTooltipOpen && (
-            <Tooltip
-              message='최근 이용극장을 선호극장에 추가해보세요'
-              handleClose={handleCloseTooltip}
-            />
-          )}
-
-          <div className='flex w-full flex-col gap-[2.2rem]'>
-            {filteredShowtimes.map(cinema => (
-              <div className='flex w-full flex-col gap-[2.2rem]'>
-                <Cinema
-                  key={cinema.cinemaName}
-                  cinemaName={cinema.cinemaName}
-                  isShowtimeOpen={showtimeOpenMap[cinema.cinemaName] ?? true}
-                  handleOpenShowtime={isOpen =>
-                    handleOpenShowtime(cinema.cinemaName, isOpen)
-                  }
+          <TimeChips
+            selectedTimeId={selectedTimeId}
+            handleClickTime={handleClickTime}
+          />
+          {isLoading ? (
+            <Spinner className='w-[15rem]' />
+          ) : (
+            <>
+              {filteredShowtimes.length !== 0 && isTooltipOpen && (
+                <Tooltip
+                  message='최근 이용극장을 선호극장에 추가해보세요'
+                  handleClose={handleCloseTooltip}
                 />
-                {showtimeOpenMap[cinema.cinemaName] &&
-                  cinema.movies.map(movie => (
-                    <>
-                      <Movie
-                        key={`${cinema.cinemaName} - ${movie.movieTitle}`}
-                        movieTitle={movie.movieTitle}
-                        ageRating={12}
-                      />
-                      {movie.theaters.map(theater => (
-                        <Theater
-                          key={`${cinema.cinemaName} - ${movie.movieTitle} - ${theater.theaterName}`}
-                          theaterName={theater.theaterName}
-                          screenType={theater.screenType}
-                          showtimes={theater.showtimes}
-                          cinemaName={cinema.cinemaName}
-                          movieTitle={movie.movieTitle}
-                          handleOpenModal={handleOpenChange}
-                        />
-                      ))}
-                    </>
-                  ))}
-              </div>
-            ))}
-          </div>
+              )}
+              {filteredShowtimes.length !== 0 ? (
+                <Schedule
+                  filteredShowtimes={filteredShowtimes ?? []}
+                  showtimeOpenMap={showtimeOpenMap}
+                  handleOpenShowtime={handleOpenShowtime}
+                  handleOpenChange={handleOpenChange}
+                  handleSelectShowtime={setSelectedShowtime}
+                />
+              ) : (
+                <div className='flex justify-center items-center w-full mt-[12rem] text-[1.3rem] text-gray-0'>상영정보가 존재하지 않습니다.</div>
+              )}
+            </>
+          )}
         </div>
-
         <Modal
           isOpen={isOpen}
           handleOpenChange={handleOpenChange}
