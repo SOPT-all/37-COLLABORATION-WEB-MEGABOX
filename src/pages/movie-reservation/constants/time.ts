b3bcbc10e23b5f2a807e1c40b517dbba0a45d7d5
sminha <sminha8590@naver.com>
2025-11-26 04:13:52 +0900

    rename: 시간대 칩 저녁 -> 18시 이후로 레이블 변경

--- src/pages/movie-reservation/constants/time.ts
+++ src/pages/movie-reservation/constants/time.ts
@@ -3,6 +3,6 @@ export type TimeType = 'MORNING' | 'AFTERNOON' | 'EVENING' | 'NIGHT';
 export const TIMES = [
   {type: 'MORNING', label: '아침'},
   {type: 'AFTERNOON', label: '오후'},
-  {type: 'EVENING', label: '저녁'},
+  {type: 'EVENING', label: '18시 이후'},
   {type: 'NIGHT', label: '심야'},
 ];
