fce62c418da6a3f70154f5b833d09f0341b68b78
huniversal <hunivers0523@naver.com>
2025-11-26 22:26:16 +0900

    refactor: 결제 페이지 툴팁 적용

--- src/pages/payment/@section/payment-method-section/components/JoongAngPayButton.tsx
+++ src/pages/payment/@section/payment-method-section/components/JoongAngPayButton.tsx
@@ -1,9 +1,25 @@
+import { useState } from 'react';
 import IconItemPlus from '@assets/components/IconItemPlus';
+import Tooltip from '@components/tooltip/Tooltip';
 import JoongAngPAY from '@../../public/assets/img-joonang-logo.svg';
 
 export default function JoongAngPayButton() {
+  const [isTooltipOpen, setIsTooltipOpen] = useState(true);
+
+  const handleCloseTooltip = () => {
+    setIsTooltipOpen(false);
+  };
+
   return (
-    <div className='mb-[1.6rem]'>
+    <div className='relative mb-[1.6rem]'>
+      {isTooltipOpen && (
+        <div className='absolute left-[1.6rem] -top-[2.5rem]'>
+          <Tooltip
+            message='자주 사용하는 카드를 등록하고 빠르게 결제하세요!'
+            handleClose={handleCloseTooltip}
+          />
+        </div>
+      )}
       <button
         type='button'
         className='flex w-full items-center justify-between rounded-[0.4rem] border border-gray-300 px-[2rem] py-[0.6rem]'
