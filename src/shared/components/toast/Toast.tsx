b0474817fd0478ca34164a67594df36f0a72dc6a
huniversal <hunivers0523@naver.com>
2025-11-26 23:03:56 +0900

    fix: 토스트 메시지 너비 수정

--- src/shared/components/toast/Toast.tsx
+++ src/shared/components/toast/Toast.tsx
@@ -9,8 +9,8 @@ export default function Toast() {
   }
 
   return (
-    <div className='animate-fade-in fixed right-[1rem] top-[6rem] left-[1rem] z-10'>
-      <div className='mx-auto flex max-w-[34.3rem] items-center justify-between gap-[1rem] rounded-[0.6rem] bg-gray-700 px-[1rem] py-[1.5rem]'>
+    <div className='animate-fade-in fixed inset-x-0 top-[6rem] z-10 px-[1.6rem] flex justify-center'>
+      <div className='flex w-full max-w-[60rem] items-center justify-between gap-[1rem] rounded-[0.6rem] bg-gray-700 px-[1.6rem] py-[1.5rem]'>
         <span className='font-subtitle1 text-white'>{toast.message}</span>
         <button onClick={hideToast} className='font-body2 text-violet-300'>
           확인
