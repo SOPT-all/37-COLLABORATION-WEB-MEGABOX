b92414eaa81482c91612f61d192ef3ae5d035b17
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-26 12:04:58 +0900

    refacor: h-max 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,24 +8,26 @@ export default function Home() {
   const { selectedMovie, items, handleClickItem, handleClickCard } = useMovie();
 
   return (
-    <div>
+    <div className='flex max-h-screen flex-col overflow-hidden'>
       <Header variant='main' />
-      <ErrorBoundary
-        fallbackRender={() => (
-          <div className='flex min-h-[60vh] items-center justify-center text-center text-gray-500'>
-            영화 정보가 없습니다.
-          </div>
-        )}
-      >
-        <Suspense fallback={<Spinner />}>
-          <HomeClient
-            selectedMovie={selectedMovie}
-            items={items}
-            handleClickItem={handleClickItem}
-            handleClickCard={handleClickCard}
-          />
-        </Suspense>
-      </ErrorBoundary>
+      <div className='flex-1 overflow-hidden'>
+        <ErrorBoundary
+          fallbackRender={() => (
+            <div className='flex min-h-[60vh] items-center justify-center text-center text-gray-500'>
+              영화 정보가 없습니다.
+            </div>
+          )}
+        >
+          <Suspense fallback={<Spinner />}>
+            <HomeClient
+              selectedMovie={selectedMovie}
+              items={items}
+              handleClickItem={handleClickItem}
+              handleClickCard={handleClickCard}
+            />
+          </Suspense>
+        </ErrorBoundary>
+      </div>
       <footer className='fixed-center right-0 bottom-0 left-0 px-[1.7rem] pb-[4.9rem]'>
         <Button variant='primary' onClick={handleClickCard}>
           바로 예매 하기
